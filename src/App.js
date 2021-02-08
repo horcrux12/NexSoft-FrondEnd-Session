e84bcf6cb69c7ae3e2b9fc80b1220f12bada860c
horcrux12 <silo.mardadi1@gmail.com>
2021-02-08 14:56:49 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  //wajib ada render
+  render() {
+    // wajib ada return
+    let hello = "Hello World!!";   
+    const nama = {nama : "admin"}
+    
+    return ( 
+      // wajib satu tag sebagai root
+
+      // <div> 
+      //   Hello world
+      // </div>
+
+      // atau
+      // <React.Fragment>
+      //     <div>Hello world</div>
+      // </React.Fragment>
+
+      // atau
+      <>
+        <div style={{ 
+          backgroundColor: 'blue',
+          color:'white'
+         }}>{hello} {nama.nama}</div>
+        <div className="divSecond">Hello world</div>
+      </>
+     );
+  }
 }
-
+ 
 export default App;
